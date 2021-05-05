c88917a1da72b7c8d7a77f65f33533c6ec65dded
axelpro04 <axellefpro@gmail.com>
2021-05-05 11:34:27 +0200

    exo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-$("#text").show();
+$(document).ready(function(){
+    // $("#text").show();
+    // $("#text").css("display", "block");
+    // $("#text").toggle();
+    $("#text").removeAttr("hidden");
+});
